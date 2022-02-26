dc22c5b42d8052ef1b8038b5fe9c0f22523e511f
Dai Nguyendo <dainguyendo@gmail.com>
2022-02-26 14:29:18 -0500

    try: Only Google provider + JWT and tokens

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,43 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+// import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../db";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    }),
+    // DiscordProvider({
+    //   clientId: process.env.DISCORD_CLIENT_ID,
+    //   clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
-    session: async ({ session, user }) => {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    session: async ({ session, token, user }) => {
       if (session?.user) {
         session.user.id = user.id;
       }
+
+      if (session?.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+
       return session;
     },
   },
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
