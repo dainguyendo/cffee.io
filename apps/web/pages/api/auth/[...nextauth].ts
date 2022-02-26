8ed7f1fea94e2b3c32d21b112039c6d129c31949
Dai Nguyendo <dainguyendo@gmail.com>
2022-02-26 17:40:41 -0500

    fix: Add back code - deployment works

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,16 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
-// import DiscordProvider from "next-auth/providers/discord";
+import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../db";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    // DiscordProvider({
-    //   clientId: process.env.DISCORD_CLIENT_ID,
-    //   clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    // }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
@@ -23,28 +23,20 @@ export default NextAuth({
       else if (url.startsWith("/")) return new URL(url, baseUrl).toString();
       return baseUrl;
     },
-    // async jwt({ token, account }) {
-    //   // Persist the OAuth access_token to the token right after signin
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //   }
-    //   return token;
-    // },
-    // session: async ({ session, token, user }) => {
-    //   if (session?.user) {
-    //     session.user.id = user.id;
-    //   }
+    session: async ({ session, token, user }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
 
-    //   if (session?.accessToken) {
-    //     session.accessToken = token.accessToken;
-    //   }
+      if (session?.accessToken) {
+        session.accessToken = token.accessToken;
+      }
 
-    //   return session;
-    // },
+      return session;
+    },
   },
   pages: {
     signIn: "/auth/signin",
   },
-  debug: true,
   secret: process.env.NEXTAUTH_SECRET,
 });
