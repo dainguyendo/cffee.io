f893f783654a26ee535b6bbb50799d7d4a947f88
Dai Nguyendo <dainguyendo@gmail.com>
2022-02-26 14:51:45 -0500

    try: No custom callbacks

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -16,26 +16,26 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    session: async ({ session, token, user }) => {
-      if (session?.user) {
-        session.user.id = user.id;
-      }
+  // callbacks: {
+  //   async jwt({ token, account }) {
+  //     // Persist the OAuth access_token to the token right after signin
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //     }
+  //     return token;
+  //   },
+  //   session: async ({ session, token, user }) => {
+  //     if (session?.user) {
+  //       session.user.id = user.id;
+  //     }
 
-      if (session?.accessToken) {
-        session.accessToken = token.accessToken;
-      }
+  //     if (session?.accessToken) {
+  //       session.accessToken = token.accessToken;
+  //     }
 
-      return session;
-    },
-  },
+  //     return session;
+  //   },
+  // },
   pages: {
     signIn: "/auth/signin",
   },
