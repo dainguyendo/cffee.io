a34928a437268878e6b363c81ba72a75cf922770
Dai Nguyendo <dainguyendo@gmail.com>
2022-02-26 12:43:56 -0500

    [test] No custom page

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -24,8 +24,8 @@ export default NextAuth({
       return session;
     },
   },
-  pages: {
-    signIn: "/auth/signin",
-  },
+  // pages: {
+  //   signIn: "/auth/signin",
+  // },
   secret: process.env.NEXTAUTH_SECRET,
 });
